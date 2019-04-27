c0e2b9d54587ef8df67110c9ef287dcc14761f3f
Marco Obinu <locurtola@tin.it>
2019-04-27 12:24:48 +0200

    Update WindowsVmRemoting.ps1

--- AzureCloudShell/WindowsVmRemoting.ps1
+++ AzureCloudShell/WindowsVmRemoting.ps1
@@ -2,7 +2,7 @@
 Enable-AzVMPSRemoting -Name "GAB2019VR-SqlVm" -ResourceGroupName "GAB2019VR-Demo-RG" -Protocol https -OsType Windows
 
 # Execute commands on a remote machine
-Invoke-AzVMCommand -Name "GAB2019VR-SqlVm" -ResourceGroupName "GAB2019VR-Demo-RG" -Scriptblock {Get-ComputerInfo} -credential (Get-Credential)
+Invoke-AzVMCommand -Name "GAB2019VR-SqlVm" -ResourceGroupName "GAB2019VR-Demo-RG" -Scriptblock { dir c:\ } -credential (Get-Credential)
 
 # Open an interactive session
 Enter-AzVM -Name "GAB2019VR-SqlVm" -ResourceGroupName "GAB2019VR-Demo-RG" -credential (Get-Credential)
